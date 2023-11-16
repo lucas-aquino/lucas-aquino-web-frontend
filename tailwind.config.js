79c6d607c989e1b5242a932dbf6542c2e0468d41
lucas-aquino <luckasakino@protonmail.com>
2023-11-16 17:23:08 -0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,21 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-linear-bg-home': 'linear-gradient(180deg, rgba(5, 11, 19, 0.00) 40.94%, #03080F 84.31%)',
       },
+      colors: {
+        'custom': {
+          'almost-black': '#02070E',
+          'black-pearl': '#0D1623',
+          'blue-zodiac': '#1A2A42',
+          'dark-blue-grey':'#243754',
+          'light-green-blue': '#4FF39B',
+          'periwinkle-blue': '#89A3FF',
+          'iceberg': '#D8EFFC'
+        }
+      }
     },
+    
   },
   plugins: [],
 }
