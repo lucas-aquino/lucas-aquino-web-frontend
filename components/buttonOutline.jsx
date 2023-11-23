2a8080bae41257cf7329ece9d991889936a8f466
lucas-aquino <luckasakino@protonmail.com>
2023-11-23 00:40:18 -0300

    Added button for social media

--- components/buttonOutline.jsx
+++ components/buttonOutline.jsx
@@ -3,8 +3,8 @@ import { jetBrainsMono } from "@/fonts/font"
 
 export default function ButtonOutline({ children, className, ...rest }) {
   return (
-    <button className={ `${ jetBrainsMono.className } ${ className } flex flex-row justify-center items-center border-4 rounded-full font-bold rounded-full px-10 py-4 tracking-wide` } { ...rest }> 
-      { children } 
+    <button className={ `${ jetBrainsMono.className } ${ className } flex flex-row justify-center items-center rounded-full rounded-full tracking-wide` } { ...rest }> 
+      { children }
     </button>
   )
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,11 +1,19 @@
 import Logo from '/components/logo.jsx'
 
+import ButtonOutline from './buttonOutline'
+
+import { FaGithub } from "react-icons/fa";
 
 export default function NavBar() {
   return(
     <nav className='flex justify-center z-40 w-full py-12'>
-      <div className='w-[1226px]'>
+      <div className='w-[1226px] flex flex-row justify-between items-center'>
         <Logo></Logo>
+        <div>
+          <ButtonOutline className='self-center h-full border-2 font-light font-extralight pl-1 py-1 pr-4'>
+            <span className='mr-1'><FaGithub size={'2.1em'} /></span>/lucas-aquino
+          </ButtonOutline>
+        </div>
       </div>
     </nav>
   )
